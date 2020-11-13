23ad255408e3d738e44bfe2923ceed51ad39f01b
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-11-13 16:45:27 +0100

    order saved in db by clicking on rent in Catalogue.jsx

--- src/containers/Catalogue/Catalogue.jsx
+++ src/containers/Catalogue/Catalogue.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { GET_MOVIES } from '../../redux/types';
 import './Catalogue.scss'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 
 const Catalogue = (props) => {
@@ -14,51 +15,102 @@ const Catalogue = (props) => {
             }).catch((err) => {
                 console.log(err)
             });
+            // eslint-disable-next-line
     }, [])
-    
-    const [search, setSearch] = useState("")
-    
-    let movies = []
 
-    movies = props.movies
+    const [search, setSearch] = useState("")
+
 
     const searchMovies = event => {
         setSearch(event.target.value)
         //console.log(event.target, search);
     }
 
-    //const [moviesFiltered, setMoviesFiltered] = useState([])
+    const [selectedMovie, setSelectedMovie] = useState({})
+
+
 
     const searchEngine = (props) => {
         const result = props.movies?.filter(movie => {
             //console.log(search, movie.title.indexOf(search))
             return movie.title.toLowerCase().indexOf(search.toLowerCase()) !== -1
         })
-        if (search != "")
-            return result.map(movie => <div>{movie.title}</div>)
+        if (search !== "")
+            return result.map(movie => <div className="movies" onClick={() => setSelectedMovie(movie)}>
+                <Button onClick={abrirModal}>+info</Button>
+                {/* <h4>{movie.title}</h4> */}
+                <img src={'https://image.tmdb.org/t/p/w185' + movie.poster_path} alt="" /></div>)
+    }
+
+    
+
+    const [modal, setModal] = useState(false)
+
+
+
+    const abrirModal = () => {
+        setModal({ abierto: !modal.abierto });
+    }
+
+    const compruebaToken = localStorage.getItem('token');
+
+   
+
+    const rentMovie = async () => {
+        
+        console.log(selectedMovie.id)
+        console.log(compruebaToken)
+        await axios.post('http://localhost:3000/orders', {movies:[selectedMovie.id]}, {  headers: {
+            Authorization: "Bearer " + compruebaToken
+        }});
+        setModal({ abierto: !modal.abierto });
     }
 
 
     return (
-        <div className="body">
-            <div className="background">
-                <div className="containerSearch">
-                    <input type="text" onKeyUp={searchMovies}></input>
-                    <div className="moviesSearched">
-                        {searchEngine(props)}
+        <>
+            <div className="body">
+                <div className="background">
+                    <div className="search">
+                        <input type="text" onKeyUp={searchMovies}></input>
                     </div>
-                </div>
-                <div className="containerAllMovies">
 
-                    <div className="allMovies">
-                        {props.movies?.map(movie =>
-                            <div className="movies">
-                                <h3>{movie.title}</h3>
-                            </div>)}
+                    <div className="containerMovies">
+
+                        <div className="titleMovies">
+                            {searchEngine(props)}
+                        </div>
+
+                        <div className="allMovies">
+                            {props.movies?.map(movie =>
+                                <div className="movies" onClick={() => setSelectedMovie(movie)}>
+                                    <Button onClick={abrirModal}>+info</Button>
+                                    {/* <h4>{movie.title}</h4> */}
+                                    <img src={'https://image.tmdb.org/t/p/w185' + movie.poster_path} alt="" />
+                                </div>)}
+                        </div>
+
                     </div>
                 </div>
+
+
             </div>
-        </div>
+
+            <Modal className="modal" isOpen={modal.abierto}>
+                <ModalHeader>
+                    <h2>{selectedMovie?.title ?selectedMovie.title:""}</h2>
+                </ModalHeader>
+
+                <ModalBody>
+                    pelicula
+            </ModalBody>
+
+                <ModalFooter>
+                    <Button onClick={rentMovie}>Alquilar</Button>
+                    <Button onClick={abrirModal}>Salir</Button>
+                </ModalFooter>
+            </Modal>
+        </>
     )
 
 }
